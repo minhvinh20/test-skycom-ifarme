b5277714906cb5f90019196a9f9ac28202c90806
HienCao <caohiendev01@gmail.com>
2023-08-30 10:23:33 +0700

    test

--- scr/js/main.js
+++ scr/js/main.js
@@ -203,6 +203,19 @@ function listenEventChangeFielsValidate() {
 }
 listenEventChangeFielsValidate();
 // ===================================================================
+function getParam(p) {
+    var match = RegExp('[?&]' + p + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+function getExpiryRecord(value) {
+    var expiryPeriod = 90 * 24 * 60 * 60 * 1000;
+    var expiryDate = new Date().getTime() + expiryPeriod;
+    return {
+        value: value,
+        expiryDate: expiryDate
+    };
+}
 function addGclid() {
     var gclidParam = getParam('gclid');
     var gclidFormFields = ['gclid_field', 'foobar'];
@@ -210,23 +223,25 @@ function addGclid() {
     var currGclidFormField;
     var gclsrcParam = getParam('gclsrc');
     var isGclsrcValid = !gclsrcParam || gclsrcParam.indexOf('aw') !== -1;
-
+    
     gclidFormFields.forEach(function(field) {
         if (document.getElementById(field)) {
             currGclidFormField = document.getElementById(field);
         }
     });
-
+    
     if (gclidParam && isGclsrcValid) {
         gclidRecord = getExpiryRecord(gclidParam);
         localStorage.setItem('gclid', JSON.stringify(gclidRecord));
     }
-
+    
     var gclid = gclidRecord || JSON.parse(localStorage.getItem('gclid'));
     var isGclidValid = gclid && new Date().getTime() < gclid.expiryDate;
     if (currGclidFormField && isGclidValid) {
         currGclidFormField.value = gclid.value;
         document.getElementById('expiry').value = gclid.expiryDate;
     }
+    console.log(gclidParam)
 }
 window.addEventListener('load', addGclid);
+console.log('windown.parent.location.href', windown.parent.location.href)

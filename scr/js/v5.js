4e5003f563405d7cc18fe8e9441e2ab70d035ada
ngovanvien1010 <ngovanvien1010@gmail.com>
2023-09-11 15:02:10 +0700

    v5 - detect show virtual keyboard

---
+++ scr/js/v5.js
@@ -0,0 +1,244 @@
+
+const bareURL = "https://backendsyncdata.skycom.vn/api/spam_check/form-spam-check";
+const urlThankReal = "https://hoaianbeauty.com/pages/cam-on-quy-khach";
+const urlThankFake = "https://hoaianbeauty.com/pages/camon-quy-khach";
+const urlSyncGoogleSheetSpam = "https://script.google.com/macros/s/AKfycbzGbJQ83uGDjEGC5x8yqbIjKr0ATrzk6gNYcIxAI958cHpSfc-r6KHnylZNK7fxCpFgaQ/exec";
+
+// =================================================================== 
+
+let encodeName = "", encodePhone = "", fe_check = false, note = '', isShowKeyboard = false, timeNa = "", timePo = "";
+const timeFirstRenderPage = new Date();
+const regexPhone = /^(0|\+84)(9[0-9]|3[2-9]|7[06-9]|5[6-9]|8[1-9]|2[0-9])\d{7}$/;
+const iOSDevice = !!navigator.platform.match(/iPhone|iPod|iPad/);
+let parentUrl = window.location.href.indexOf("split=") > -1 ? window.location.href.split("split=")[1] : window.top.location.href;
+
+const inputCache = document.querySelectorAll(".input-cache .div-input");
+const form = document.getElementById("form1");
+
+if ('visualViewport' in window) {
+    window.visualViewport.addEventListener('resize', function (event) {
+        isShowKeyboard = true;
+    });
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+const number = getRandomInt(100);
+
+function textEnpty(e) {
+    let _str = " ";
+    return _str.repeat(e);
+}
+
+// ===================================================================
+function encryptionIDFileds() {
+    inputCache.forEach((element, index) => {
+        const randomString = (Math.random() + 1).toString(36).slice(2, 7);
+        if (index === 0) {
+            encodeName = randomString;
+        }
+        if (index === 1) {
+            encodePhone = randomString;
+        }
+        let placeholder = element.getAttribute("placeholder");
+        placeholder += textEnpty(100) + randomString + "-" + randomString + textEnpty(getRandomInt(20)) + randomString + "-" + randomString;
+        element.setAttribute("id", randomString);
+        element.setAttribute("name", randomString);
+        // element.setAttribute("placeholder", placeholder);
+    });
+}
+encryptionIDFileds();
+// ===================================================================
+
+function validateForm() {
+    const valuePhone = document.getElementById(`${encodePhone}`).innerText;
+    let validate = true;
+
+    if (!regexPhone.test(valuePhone)) {
+        document.getElementById("errorMessage").innerText = "Vui lòng nhập đúng định dạng số điện thoại";
+        validate = false;
+    }
+    return validate;
+}
+const syncToSheetServerFail = async ({ name, phone, address, ad_channel, ad_account, link, reasons }) => {
+    link = link.indexOf("&") > -1 ? link.replaceAll("&", "_SKYCOM_") : link;
+    await fetch(
+        `${urlSyncGoogleSheetSpam}?time=${timeFirstRenderPage.toLocaleDateString()}-${timeFirstRenderPage.toLocaleTimeString()}&name=${name}&phone=${phone}&utmSource=${ad_channel || ''}&utmMedium=${ad_account || ''}&link=${link}&reasons=${reasons}&SHEET_NAME=ErrorServer`,
+        {
+            method: "GET",
+            mode: "no-cors",
+            headers: { "Content-Type": "application/json" },
+            redirect: "follow",
+        }
+    );
+};
+const syncToSheetDataSubmit = async ({ name, phone, address, ad_channel, ad_account, link, spam, reasons }) => {
+    link = link.indexOf("&") > -1 ? link.replaceAll("&", "_SKYCOM_") : link;
+    await fetch(
+        `${urlSyncGoogleSheetSpam}?time=${timeFirstRenderPage.toLocaleDateString()}-${timeFirstRenderPage.toLocaleTimeString()}&name=${name}&phone=${phone}&utmSource=${ad_channel || ''}&utmMedium=${ad_account || ''}&link=${link}&spam=${spam}&reasons=${reasons}&SHEET_NAME=TotalSDT`,
+        {
+            method: "GET",
+            mode: "no-cors",
+            headers: { "Content-Type": "application/json" },
+            redirect: "follow",
+        }
+    );
+};
+const handlePostData = async ({ Ten1, Ten2, name, phone, fe_check, note, time }) => {
+    const params = { Ten1, Ten2, name, phone, fe_check, note, link: parentUrl, actionTime: time };
+    const response = await fetch(bareURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            syncToSheetDataSubmit({
+                name: Ten1,
+                phone: Ten2,
+                link: parentUrl,
+                spam: data.spam,
+                reasons: data.reasons,
+                ad_channel: data.ad_channel,
+                ad_account: data.ad_account,
+            });
+            if (data.spam) {
+                window.parent.location.replace(urlThankFake);
+            } else {
+                window.parent.location.replace(`${urlThankReal}?name=${Ten1}&phone=${Ten2}}`);
+            }
+        })
+        .catch((error) => {
+            syncToSheetServerFail({
+                name: Ten1,
+                phone: Ten2,
+                link: parentUrl,
+                reason: error.message,
+                ad_channel: data.ad_channel,
+                ad_account: data.ad_account,
+            });
+        });
+};
+
+function handleSubmit() {
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        checkCookieDisable();
+        handleCheckShowKeyboard();
+        const invalid = validateForm();
+        if (invalid) {
+            const { action_na_time, action_po_time } = inputTiming();
+            const timeClickBuy = Math.round(Math.abs(new Date() - timeFirstRenderPage) / 1000);
+            const Ten1 = document.getElementById(`${encodeName}`).value;
+            const Ten2 = document.getElementById(`${encodePhone}`).innerText;
+            const name = document.getElementById("ten2").value;
+            const phone = document.getElementById("sdt2").value;
+            const buttonSubmit = document.getElementById("btn-submit");
+            buttonSubmit.innerText = "ĐANG XỬ LÝ!!!";
+            buttonSubmit.parentElement.classList.add("disable");
+            handlePostData({ Ten1, Ten2, name, phone, fe_check, note, action_na_time, action_po_time, time: timeClickBuy });
+        }
+    });
+}
+handleSubmit();
+// ===================================================================
+const inputTiming = () => {
+    let timeNa = "", timePo = "";
+    document.getElementById(encodeName).addEventListener('focus', () => {
+        if (timeNa == '') { timeNa = new Date(); }
+    });
+    document.getElementById(encodePhone).addEventListener('focus', () => {
+        if (timePo == '') { timePo = new Date(); }
+    });
+
+    const action_po_time = Math.round(Math.abs(new Date() - timePo) / 1000);
+    const action_na_time = Math.round(Math.abs(new Date() - timeNa) / 1000);
+
+    return { action_po_time, action_na_time }
+}
+// ===================================================================
+const handleCheckShowKeyboard = () => {
+    if (isShowKeyboard) {
+        fe_check = false;
+    } else {
+        fe_check = true;
+        note = "Bot không sử dụng bàn phím"
+    }
+}
+// ===================================================================
+
+function disableCopy() {
+    document.getElementById(encodeName).onpaste = (e) => e.preventDefault();
+    document.getElementById(encodePhone).onpaste = (e) => e.preventDefault();
+}
+disableCopy();
+
+// ===================================================================
+function disableEnterSubmit() {
+    document.addEventListener("keydown", (e) => {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            return;
+        }
+    });
+}
+disableEnterSubmit();
+
+// ===================================================================
+async function syncToSheetValidate({ phone, link }) {
+    link = link.indexOf("&") > -1 ? link.replaceAll("&", "_SKYCOM_") : link;
+    await fetch(
+        `${urlSyncGoogleSheetSpam}?phone=${phone}&link=${link}&SHEET_NAME=SDTValidate`,
+        {
+            method: "GET",
+            mode: "no-cors",
+            headers: { "Content-Type": "application/json" },
+            redirect: "follow",
+        }
+    );
+}
+function listenEventChangeFielsValidate() {
+    const fieldPhone = document.getElementById(encodePhone);
+    fieldPhone.addEventListener("input", (e) => {
+        if (regexPhone.test(e.target.value)) {
+            syncToSheetValidate({ phone: e.target.value, link: parentUrl });
+        }
+    });
+}
+listenEventChangeFielsValidate();
+
+// ===================================================================
+const checkCookieDisable = () => {
+    if (iOSDevice) {
+        return;
+    }
+    let cookieEnabled = navigator.cookieEnabled;
+    if (!cookieEnabled) {
+        document.cookie = "skycomForm=skycom";
+        cookieEnabled = document.cookie.indexOf("skycomForm") != -1;
+    }
+    if (!cookieEnabled) {
+        fe_check = true;
+        note = 'Detect disable cookie'
+    }
+}
+
+// ===================================================================
+const checkProxyEnable = () => {
+    const proxyHeader = 'via';
+    let req = new XMLHttpRequest();
+    req.open('GET', window.location, false);
+    req.send();
+    let header = req.getResponseHeader(proxyHeader);
+    if (header) {
+        checkProxy = true,
+            note = 'Detect disable Proxy'
+    }
+}
+checkProxyEnable();

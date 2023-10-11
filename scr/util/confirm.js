d7d383177126fdc56483ddcd52ed83e4c791e0ee
HienCao <caohiendev01@gmail.com>
2023-10-11 15:02:58 +0700

    update rule cofirm

---
+++ scr/util/confirm.js
@@ -0,0 +1,43 @@
+import { apis } from "../config/apis";
+
+let element = {
+    form: null,
+    input: null,
+}
+let lead_id = window.location.href.split("=")[1] || null;
+
+document.addEventListener("DOMContentLoaded", function () {
+    //query element
+    mountForm();
+    //submit
+    element.form.addEventListener('submit', e => {
+        let phone = element.input?.value;
+        handleconfirmPhone(lead_id,phone)
+    })
+})
+const mountForm = () => {
+    element.form = document.querySelector("form");
+    element.input = document.querySelector("form input");
+};
+const handleconfirmPhone = async () =>{
+    if(!lead_id) return;
+    const params = {lead_id,phone }
+    await fetch(apis.confirm, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then((data) => {
+        if(data.passed) {
+            window.parent.location.replace(`${apis.habt.urlThankReal}?name=${data.name}&phone=${data.phone}`);
+        }else{
+            window.parent.location.replace(`${apis.habt.urlThankFake}?name=${data.name}&phone=${data.phone}`);
+        }
+       console.log('data', data)
+    })
+}

a9ff9ed5649b6c3ba3643d43d18743cfe8b1d5fe
HienCao <caohiendev01@gmail.com>
2023-10-23 09:12:49 +0700

    test ggsheet

--- scr/config/apis.js
+++ scr/config/apis.js
@@ -4,7 +4,7 @@ export const apis = {
     urlThankReal: "https://hoaianbeauty.com/pages/cam-on-quy-khach",
     urlThankFake: "https://hoaianbeauty.com/pages/camon-quy-khach",
     urlSyncGoogleSheetSpam:
-      "https://script.google.com/macros/s/AKfycbyL4_P2iubW-0kLfWBgC5y6y2WZ-vNBpo-Yhz4Ke94TfEgokcgJnErb_cNn6c07vKa6/exec",
+      "https://script.google.com/macros/s/AKfycbzS2nq5H7R8gf8LV_IB4GEqNABUi1LPpvYsME_RS8uD88JXwQ1_WKSg8FGCzuSjYHyt/exec",
     urlConfirm: "https://hoaianbeauty.com/pages/xac-nhan-thong-tin",
   },
   jp24: {
@@ -12,7 +12,7 @@ export const apis = {
     urlThankReal: "https://sp.jp24.vn/camon",
     urlThankFake: "https://sp.jp24.vn/cam-on",
     urlSyncGoogleSheetSpam:
-      "https://script.google.com/macros/s/AKfycbyL4_P2iubW-0kLfWBgC5y6y2WZ-vNBpo-Yhz4Ke94TfEgokcgJnErb_cNn6c07vKa6/exec",
+      "https://script.google.com/macros/s/AKfycbzS2nq5H7R8gf8LV_IB4GEqNABUi1LPpvYsME_RS8uD88JXwQ1_WKSg8FGCzuSjYHyt/exec",
     urlConfirm: "https://jp24.vn/demo-confirm",
   },
   sago: {
@@ -20,7 +20,7 @@ export const apis = {
     urlThankReal: "https://www.daugoisaryyam.com/camon",
     urlThankFake: "https://www.ryusui.site/cam-on",
     urlSyncGoogleSheetSpam:
-      "https://script.google.com/macros/s/AKfycbyL4_P2iubW-0kLfWBgC5y6y2WZ-vNBpo-Yhz4Ke94TfEgokcgJnErb_cNn6c07vKa6/exec",
+      "https://script.google.com/macros/s/AKfycbzS2nq5H7R8gf8LV_IB4GEqNABUi1LPpvYsME_RS8uD88JXwQ1_WKSg8FGCzuSjYHyt/exec",
   },
   visitorID: "https://fingerprint.skycom.vn/api/v1/visits/",
   confirm: "https://devsyncdata.skycom.vn/api/spam_check/cf-phone-numbers",
